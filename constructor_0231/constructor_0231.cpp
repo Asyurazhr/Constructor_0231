4794fc203efcbae8e27a4daa5615b7ab9d69bb10
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-14 14:45:12 +0700

    create fungsi utama

--- constructor_0231/constructor_0231.cpp
+++ constructor_0231/constructor_0231.cpp
@@ -18,11 +18,24 @@ public:
     }
 };
 
+int mahasiswa::nim = 231;
+
+void mahasiswa::setID()
+{
+    id = ++nim;
+}
+
+void mahasiswa::printAll()
+{
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
 
