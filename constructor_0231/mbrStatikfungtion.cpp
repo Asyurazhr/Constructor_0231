237e32735ce547b4fb6198589d3fdb9d750d5faa
Asyurazhr <Asyuradjola2004@gmail.com>
2024-05-14 15:19:48 +0700

    create fungsi utama

--- constructor_0231/mbrStatikfungtion.cpp
+++ constructor_0231/mbrStatikfungtion.cpp
@@ -39,4 +39,21 @@ void mahasiswa::printAll()
 	cout << endl;
 }
 
+int main()
+{
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setnim(231);
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
+
+	return 0;
+}
 
